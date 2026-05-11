b2c93acf1e3a03818daf92255f70a348213c982c
Creator Vishal <Vishalsharma805966@gmail.com>
2026-05-11 16:44:43 +0530

    Setup Entity Framework Core with Student entity and DbContext

---
+++ EFPractice/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EFPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFPractice.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=EFCoreDb;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ EFPractice/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace EFPractice.Models
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
