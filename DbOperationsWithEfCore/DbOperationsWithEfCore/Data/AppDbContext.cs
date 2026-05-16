00e1ffd1d4ac61eae6ec005bd2a57f3904479252
Creator Vishal <Vishalsharma805966@gmail.com>
2026-05-16 17:56:24 +0530

    Added EF Core project

---
+++ DbOperationsWithEfCore/DbOperationsWithEfCore/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsWithEfCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
+    }
+}
