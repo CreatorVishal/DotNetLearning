812d81382d496e4b5782b82658759b8132ef0256
Creator Vishal <Vishalsharma805966@gmail.com>
2026-05-17 19:04:24 +0530

    Updated entity validation and migrations

--- DbOperationsWithEfCore/DbOperationsWithEfCore/Data/Book.cs
+++ DbOperationsWithEfCore/DbOperationsWithEfCore/Data/Book.cs
@@ -1,10 +1,18 @@
-﻿namespace DbOperationsWithEfCore.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace DbOperationsWithEfCore.Data
 {
     public class Book
     {
         public int Id { get; set; }
-        public string Title  { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Same { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Title  { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Range(1, 5000)]
         public int NoOfPages { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedOn { get; set; }
