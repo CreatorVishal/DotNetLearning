8ee50e60235a3ddc97f46954a1154f72b08dc1e6
Creator Vishal <Vishalsharma805966@gmail.com>
2026-06-19 15:48:35 +0530

    .

---
+++ GymManagementApi/Controllers/MemberController.cs
@@ -0,0 +1,25 @@
+﻿using GymManagementApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MemberController : ControllerBase
+{
+    [HttpGet]
+    public Member Get()
+    {
+        return new Member
+        {
+            Id = 1,
+            Name = "Vishal",
+            Age = 22
+        };
+    }
+    [HttpGet("{id}")]
+    public string GetById(int id)
+    {
+        return $"Member Id = {id}";
+    }
+}
---
+++ GymManagementApi/Models/Member.cs
@@ -0,0 +1,11 @@
+﻿namespace GymManagementApi.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
