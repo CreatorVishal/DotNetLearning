49ef53230fff108f46d56e5924d1be1a9d03dd64
Creator Vishal <Vishalsharma805966@gmail.com>
2026-07-26 11:54:06 +0530

    Revise Asp.net core concepts

--- BankingSystemApi/Controllers/AccountsController.cs
+++ BankingSystemApi/Controllers/AccountsController.cs
@@ -6,8 +6,31 @@ namespace BankingSystemApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    
-    public class AccountsController:ControllerBase
+    // Step 1 : Receive Request
+
+    // Step 2 : Call Service
+
+    // Step 3 : Get Response
+
+    // Step 4 : Return Response
+
+    //-----------------------------
+    //Result class
+    //Results.Ok();
+
+    //Results.NotFound();
+
+    //Results.BadRequest();
+
+    //Results.Created();
+
+    //Results.NoContent();
+
+    //Results.Unauthorized();
+
+    //Results.Json();
+
+    public class AccountsController : ControllerBase
     {
         private readonly IAccountService _accountService;
         private readonly BankSettings _bankSettings;
@@ -24,5 +47,61 @@ namespace BankingSystemApi.Controllers
             var result = _accountService.GetAllAccounts();
             return Ok($"{bankName} - {result}");
         }
+        [HttpGet("Success")] //200
+        public IActionResult Success()
+        {
+            return Ok("Success");
+        }
+        [HttpGet("notfound")] //404
+        public IActionResult NotFound()
+        {
+            return NotFound("Not Found");
+        }
+        [HttpGet("badrequest")] //400
+        public IActionResult BadRequestDemo()
+        {
+            return BadRequest("Invalid Request");
+        }
+
+        [HttpGet("nocontent")] //204
+        public IActionResult NoContentDemo()
+        {
+            return NoContent();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetElementById(int id)
+        {
+            return Ok(id);
+        }
+        [HttpPost]
+        public IActionResult CreateAccount()
+        {
+            return Ok("Account Created");
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateAccount(int id)
+        {
+            return Ok($"Account {id} updated");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAccount(int id)
+        {
+            return Ok($"Account {id} Deleted");
+        }
+        //Query parameter
+        [HttpGet("Search")]
+        public IActionResult SearchAccount(string name)
+        {
+            return Ok($"Searching : {name}");
+        }
+        [HttpGet("Filter")]
+        public IActionResult Filter([FromQuery]string city , bool isActive)
+        {
+            return Ok($"{city} {isActive}");
+        }
     }
 }
+
+
+
+
--- BankingSystemApi/Models/Account.cs
+++ BankingSystemApi/Models/Account.cs
@@ -1,14 +1,25 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace BankingSystemApi.Models
 {
     public class Account
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
         public string AccountNumber { get; set; }= string.Empty;
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string AccountHolderName { get; set; } = string.Empty;
+        [Range(1000, 10000000)]
         public decimal Balance { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required]
+        [Phone]
         public string PhoneNumber { get; set; }= string.Empty;
         public DateTime CreatedAt { get; set; }
         public Boolean IsActive { get; set; }
